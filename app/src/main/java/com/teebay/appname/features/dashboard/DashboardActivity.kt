e4ebe64283a7d8cfac2a05ac04d12270251c5921
sudipta-chy <anikc795@gmail.com>
2025-07-20 00:20:19 +0600

    added dashboard screen

--- app/src/main/java/com/teebay/appname/features/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/teebay/appname/features/dashboard/DashboardActivity.kt
@@ -5,17 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.teebay.appname.R
+import com.teebay.appname.databinding.ActivityDashboardBinding
+import com.teebay.appname.features.allProduct.AllProductFragment
+import com.teebay.appname.features.myProduct.MyProductFragment
+import com.teebay.appname.features.profile.ProfileFragment
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
+
+    private val allProductFragment = AllProductFragment()
+    private val myProductFragment = MyProductFragment()
+    private val profileFragment = ProfileFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+
+        showFragment(allProductFragment)
+
+        binding.bottomNav.setOnItemSelectedListener {
+            val fragment = when(it.itemId) {
+                R.id.miAllProduct -> allProductFragment
+                R.id.miMyProduct -> myProductFragment
+                R.id.miProfile -> profileFragment
+                else -> null
+            }
+
+            fragment?.let { showFragment(it) }
+
+            true
+        }
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragContainer, fragment)
+            .commit()
     }
 }
