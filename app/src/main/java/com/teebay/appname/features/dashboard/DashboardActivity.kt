a6da69e248904a70544b0a76b494e98c1db596c6
sudipta-chy <anikc795@gmail.com>
2025-07-24 02:00:08 +0600

    updated bottom nav visibility for product details

--- app/src/main/java/com/teebay/appname/features/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/teebay/appname/features/dashboard/DashboardActivity.kt
@@ -1,6 +1,7 @@
 package com.teebay.appname.features.dashboard
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,7 @@ import androidx.navigation.ui.NavigationUI
 import com.teebay.appname.R
 import com.teebay.appname.databinding.ActivityDashboardBinding
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.core.view.isVisible
 
 @AndroidEntryPoint
 class DashboardActivity : AppCompatActivity() {
@@ -26,7 +28,12 @@ class DashboardActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                if (binding.bottomNav.isVisible) 0 else systemBars.bottom
+            )
             insets
         }
 
@@ -34,5 +41,12 @@ class DashboardActivity : AppCompatActivity() {
         navController = navHostFragment.navController
 
         NavigationUI.setupWithNavController(binding.bottomNav, navController)
+
+        navController.addOnDestinationChangedListener { _,destination,_ ->
+            when(destination.id) {
+                R.id.productDetailsFragment -> binding.bottomNav.visibility = View.GONE
+                else -> binding.bottomNav.visibility = View.VISIBLE
+            }
+        }
     }
 }
